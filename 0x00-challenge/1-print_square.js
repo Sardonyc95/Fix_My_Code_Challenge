4635b8155740758d06f0ee60f698696b3d3edf84
Amavic <amarachi5695@gmail.com>
2024-01-28 05:38:51 -0800

    Print square

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -14,11 +14,11 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-let size = parseInt(process.argv[2]);
-
+const size = parseInt(process.argv[2]);
 if (isNaN(size) || size <= 0) {
 	process.stderr.write("Invalid size argument\n");
 	process.exit(1);
+}
 
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
